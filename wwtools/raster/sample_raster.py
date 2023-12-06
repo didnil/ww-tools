7df68929bf463d4c89364551711cfe17163b356a
didnil <nilsson.didrik@outlook.com>
2023-12-06 13:55:22 +0100

    Added docstrings

--- wwtools/raster/sample_raster.py
+++ wwtools/raster/sample_raster.py
@@ -7,6 +7,18 @@ from rasterio.mask import mask
 
 # Tilldelar polygon-datasetet med de samplade värdena
 def set_sampled_values(df, sampled_values):
+    """
+    Assign the calculated statistics to the DataFrame
+
+    Parameters
+    -----------
+    df : GeoDataFrame
+        DataFrame with polygons
+    sampled_values : dict
+        Dictionary with indices and values to assign
+
+
+    """
     for stat in ["max", "min", "average"]:
         df.loc[:, stat] = df.index.map(
             {key: val[stat] for key, val in sampled_values.items()}
@@ -14,37 +26,40 @@ def set_sampled_values(df, sampled_values):
     return df
 
 
-def zonal_statistics(polygons, elev_data):  # , statistics=['max', 'min', 'average']):
-    # Loopa igenom alla polygoner och välja ut raster-värdena inom varje polygon
-    # sedan filtreras väredna ut som inte är nodata
-    # varefter statistik beräknas
+def zonal_statistics(polygons, raster_data, statistics=["max", "min", "average"]):
+    """
+    Calculate raster statistics within polygons
+
+    Parameters
+    -----------
+    polygons : GeoDataFrame
+        DataFrame with polygons
+    raster_data : Raster
+        Raster with the values to be sampled. The values are assumed to be within layer 1
+    statistics : list
+        List with the statistics to calculate. Currently max, min and average are supported
+
+    """
 
     update_values = dict()
     for idx in polygons.index:
         filtered_raster, filtered_raster_transform = mask(
-            elev_data, [polygons.loc[idx, "geometry"]], crop=True
+            raster_data, [polygons.loc[idx, "geometry"]], crop=True
         )
-        filtered_raster_meta = elev_data.meta
-        filtered_values = filtered_raster[filtered_raster > elev_data.meta["nodata"]]
+        filtered_raster_meta = raster_data.meta
+        filtered_values = filtered_raster[filtered_raster > raster_data.meta["nodata"]]
         update_values[idx] = {
             "max": np.max(filtered_values),
             "min": np.min(filtered_values),
             "average": np.average(filtered_values),
         }
-        # print(f"Polygon: {idx}\nMax: {np.max(filtered_values)}\nMin: {np.min(filtered_values)}\n-----------------------\n")
 
     polygons = set_sampled_values(polygons, update_values)
-    return polygons
+    return polygons.loc[:, statistics]
+
 
 # %% [markdown]
 # ## Koden nedan kan användas om man vill spara de klippa lagren också
-#
-# ```python
-# for idx in polygons.index:
-#     filtered_raster, filtered_raster_transform = mask(elev_data, [polygons.loc[idx, 'geometry']], crop=True)
-#     filtered_raster_meta = elev_data.meta
-#     filtered_values = filtered_raster[filtered_raster>elev_data.meta['nodata']]
-#     print(f"Polygon: {idx}\nMax: {np.max(filtered_values)}\nMin: {np.min(filtered_values)}\n-----------------------\n")
 #
 #     filtered_raster_meta.update({"driver": "GPKG",
 #                                  "height": filtered_raster.shape[1],
